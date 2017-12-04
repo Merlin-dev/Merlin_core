080cadca1005998469451f09f8f41b510509b704
Lukáš Jech <klukule@moowdesign.eu>
2017-12-04 16:06:32 +0100

    Added ExecuteScript packet

---
+++ Merlin.Communication/Packet/ExecuteScript.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Merlin.Communication
+{
+    /// <summary>
+    /// Loads and executes DLL, and launches the script
+    /// </summary>
+    public class ExecuteScript
+    {
+        /// <summary>
+        /// The script name
+        /// </summary>
+        public string ScriptName;
+        /// <summary>
+        /// The path to DLL
+        /// </summary>
+        public string AssemblyPath;
+        //TODO: Some configuration?
+    }
+}
