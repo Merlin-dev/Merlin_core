108c56efc593cb4c66b13f801f68f5fc47b77904
Lukáš Jech <klukule@moowdesign.eu>
2017-12-06 11:33:10 +0100

    Added base "template" for Injector, when we'll use more injectors for more games/platforms

--- Merlin.Communication/Packet/ExecutePlugin.cs
+++ Merlin.Communication/Packet/ExecutePlugin.cs
@@ -6,18 +6,22 @@ using System.Text;
 namespace Merlin.Communication
 {
     /// <summary>
-    /// Loads and executes DLL, and launches the script
+    /// Loads and executes DLL, and launches the scripts
     /// </summary>
     public class ExecuteScript
     {
         /// <summary>
-        /// The script name
+        /// The plugin name
         /// </summary>
-        public string ScriptName;
+        public string PluginName;
         /// <summary>
         /// The path to DLL
         /// </summary>
         public string AssemblyPath;
-        //TODO: Some configuration?
+
+        /// <summary>
+        /// The assembly qualified names of the scripts
+        /// </summary>
+        public string[] ScriptAssemblyQualifiedNames;
     }
 }
---
+++ Merlin.Injector.Minject/Minject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merlin.Injector
+{
+    public class Minject //:IInjector
+    {
+        public void Inject(string file)
+        {
+            //TODO: Implement whole framework for multiple injectors
+        }
+
+        public void InjectHidden(string file)
+        {
+
+        }
+
+        public void HideLastLoadedAssembly()
+        {
+
+        }
+    }
+}
