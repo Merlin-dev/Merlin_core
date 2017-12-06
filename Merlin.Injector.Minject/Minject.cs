007761b0d4bc3708cd077c2bbf13c13b06e3087e
Lukáš Jech <klukule@moowdesign.eu>
2017-12-06 19:36:36 +0100

    Implement MInject "wrapper"

--- Merlin.Injector.Minject/Minject.cs
+++ Merlin.Injector.Minject/Minject.cs
@@ -1,26 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using MInject;
+using System.IO;
 
 namespace Merlin.Injector
 {
-    public class Minject //:IInjector
+    public class Minject
     {
-        public void Inject(string file)
+        /// <summary>
+        /// Injects the DLL to specific process.
+        /// </summary>
+        /// <param name="process">The process.</param>
+        /// <param name="file">The file.</param>
+        /// <param name="hidden">if set to <c>true</c> file will be hidden from AssemblyLoad callback.</param>
+        /// <returns>        
+        ///   <c>true</c> injection was successfull; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool Inject(Process process, string file, bool hidden = false)
         {
-            //TODO: Implement whole framework for multiple injectors
+            if(MonoProcess.Attach(process, out MonoProcess mp))
+            {
+                IntPtr domain = mp.GetRootDomain();
+                mp.ThreadAttach(domain);
+                mp.SecuritySetMode(0);
+                if (hidden)
+                {
+                    mp.DisableAssemblyLoadCallback();
+                }
+
+                byte[] rawAssembly = File.ReadAllBytes(file);
+
+                IntPtr imagePtr = mp.ImageOpenFromDataFull(rawAssembly);
+                IntPtr assemblyPtr = mp.AssemblyLoadFromFull(imagePtr);
+                if (hidden)
+                {
+                    mp.EnableAssemblyLoadCallback();
+                }
+
+                mp.Dispose();
+
+                if(assemblyPtr == IntPtr.Zero)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            return false;
         }
 
-        public void InjectHidden(string file)
+        /// <summary>
+        /// Hides the assembly loaded in <paramref name="load" /> method.
+        /// </summary>
+        /// <param name="process">The process.</param>
+        /// <param name="load">The method.</param>
+        public static bool HideAssemblyLoad(Process process, Action load)
         {
+            if (!MonoProcess.Attach(process, out MonoProcess mp))
+                return false;
 
-        }
+            IntPtr domain = mp.GetRootDomain();
+            mp.ThreadAttach(domain);
+            mp.SecuritySetMode(0);
 
-        public void HideLastLoadedAssembly()
-        {
+            if (!mp.DisableAssemblyLoadCallback())
+            {
+                mp.Dispose();
+                return false;
+            }
 
+            load();
+
+            mp.EnableAssemblyLoadCallback();
+            mp.Dispose();
+            return true;
         }
     }
 }
